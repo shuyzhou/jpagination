44b92daa8ca48d17e9dd677331a9fb4f909e89ef
shuyzhou <big_zhou@126.com>
2017-01-12 20:03:41 +0800

    add set method:two_hearts:

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# Plugin for jQuery
-name:jPagination
+# jQuery Pagination Plugin
+name:jPagination      
 author:shuyzhou
+# Get Started
+There is a example to show how to use the plugin for pagination.     
+In the HTML file, you should add only one DOM element to be the container of pagination.
+```
+<div id="pagination"></div>
+```
+Here is how to init the config of pagination.
+```
+$("#pagination").jPagination(['init',]{
+	currentPage : 7 ,
+	maxPage : 25 ,
+	callback: function (page) {
+		$("#result").html("change to page : "+page+"!");
+	}
+});
+```
 # License
-Copyright 2017, shuyzhou
+Copyright 2017, shuyzhou       
 Licensed under the MIT license.
--- js/jquery.jpagination.js
+++ js/jquery.jpagination.js
@@ -1,146 +1,176 @@
 /*!
- * JPagination Plugin for jQuery
+ * A jQuery Pagination Plugin
  * Copyright 2017, shuyzhou
  * Licensed under the MIT license.
 */
-
+var JPagination = function () {
+    this.prev = $('<a href="#" class="previous" data-action="previous">上一页</a>');
+    this.next = $('<a href="#" class="next" data-action="next">下一页</a>');
+    this.leftDot = $('<span class="dot" style="display:none">...</span>');
+    this.rightDot = $('<span class="dot" style="display:none">...</span>');
+    this.items = $('<span class="items"></span>');
+    this.input = $('<input class="input">');
+    this.confirm = $('<button class="confirm">确定</button>');
+}
+JPagination.prototype = {
+    constructure : JPagination,
+    isNumber : function (num) {
+      return !isNaN(num) && isFinite(num);
+    },
+    isFunction : function(fn) {
+      return Object.prototype.toString.call(fn)=== '[object Function]';
+    },
+    toCertainPage : function () {
+      var page = parseInt(this.input.val());
+      if(this.isNumber(page) && page > 0 && page <= this.options.maxPage){
+        this.options.currentPage = page;
+        this.refreshItems();
+        this.options.callback(page);
+      }
+    },
+    init : function (ele) {
+      var self = this;
+      //validation for options
+      if(!this.isNumber(this.options.maxPage = parseInt(this.options.maxPage)) || this.options.maxPage < 1){
+        this.options.maxPage = 1;
+      }
+      if(!this.isNumber(this.options.currentPage = parseInt(this.options.currentPage)) || this.options.currentPage < 1 || this.options.currentPage > this.options.maxPage){
+        this.options.currentPage = 1;
+      }
+      if(!this.isFunction(this.options.callback)){
+        this.options.callback = function () {};
+      }
+      this.total = $('<span class="text"></span>');
+      //init container
+      this.clear(ele);
+      ele.append(this.prev,this.leftDot,this.items,this.rightDot,this.next,'<span class="total">共</span>',this.total,'<span class="text">页,</span><span class="text">到第</span>',this.input,'<span class="text">页</span>',this.confirm);
+      //refresh pages
+      this.refreshItems();
+      //handlers
+      ele.on("click","a",function (event) {
+        var action = this.dataset.action,
+          page;
+        switch (action){
+          case "previous" :
+            page = self.options.currentPage - 1;break;
+          case "next" :
+            page = self.options.currentPage + 1;break;
+          default :
+            if(!isNaN(action = parseInt(action))){
+              page = action;
+            }
+        }
+        if(page >= 1 && page <= self.options.maxPage && page != self.options.currentPage){
+          self.options.currentPage = page;
+          self.refreshItems();
+          self.options.callback(page);
+        }
+        event.preventDefault();
+      });
+      this.input.on("keypress",function () {
+        if(event.keyCode == "13"){
+          self.toCertainPage();
+        }
+      });
+      this.confirm.on("click",function () {
+        self.toCertainPage();
+      });
+    },
+    refreshItems : function () {
+      var i,
+          start,
+          end,
+          page = this.options.currentPage;
+      this.total.html(this.options.maxPage);
+      //only display five pages
+      if(this.options.maxPage <= 5) {
+        this.displayItem(1,this.options.maxPage);
+      }
+      else {
+        start = page - 2;
+        end = page + 2;
+        while(start < 1){
+          start++;
+          end++;
+        }
+        while(end > this.options.maxPage){
+          start--;
+          end--;
+        }
+        if(start > 1){
+          this.leftDot.css("display","inline");
+        }
+        else {
+          this.leftDot.css("display","none");
+        }
+        if(end < this.options.maxPage){
+          this.rightDot.css("display","inline");
+        }
+        else {
+          this.rightDot.css("display","none");
+        }
+        this.displayItem(start,end);
+      }
+      this.options.currentPage = page;
+      //改变样式
+      if(page == 1){
+        this.prev.addClass("disabled");
+      }
+      else {
+        this.prev.removeClass("disabled");
+      }
+      if(page == this.options.maxPage){
+        this.next.addClass("disabled");
+      }
+      else {
+        this.next.removeClass("disabled");
+      }
+      $('[data-action="'+page+'"]').addClass("disabled");
+    },
+    displayItem : function (start,end) {
+      var content = '';
+      for (var i = start; i <= end; i++) {
+        content += this.options.item.replace(/\{page\}/g,i);
+      }
+      this.items.html(content);
+    },
+    clear : function (ele) {
+      ele.empty();
+      ele.off('click','a');
+    }
+};
 (function( $ ) {
-  $.fn.jPagination = function(options) {
-  	var self = this;
-  	var defaultOption = {
-  		currentPage : 1,
-  		maxPage : 1,
-  		item : '<a href="#" class="item" data-action="{page}">{page}</a>',
-  		callback : function () {}
+	//instance for pagination
+    var jPagination = new JPagination();
+    var methods = {
+    init : function (options) {
+          var ele = this;
+          var defaultOption = {
+            currentPage : 1,
+            maxPage : 1,
+            item : '<a href="#" class="item" data-action="{page}">{page}</a>',
+            callback : function () {}
+          };
+          jPagination.options = $.extend(defaultOption,options);
+          jPagination.init(ele);
+    	},
+    set : function (options) {
+          jPagination.options = $.extend(jPagination.options,options);
+          jPagination.refreshItems();
+    	}
   	};
-  	var prev = $('<a href="#" class="previous" data-action="previous">上一页</a>');
-  	var next = $('<a href="#" class="next" data-action="next">下一页</a>');
-  	var leftDot = $('<span class="dot" style="display:none">...</span>');
-  	var rightDot = $('<span class="dot" style="display:none">...</span>');
-  	var items = $('<span class="items"></span>');
-  	var total;
-  	var input = $('<input class="input">');
-  	var confirm = $('<button class="confirm">确定</button>');
-
-  	var isNumber = function (num) {
-  		return !isNaN(num) && isFinite(num);
-  	};
-  	var isFunction = function(fn) {
-   		return Object.prototype.toString.call(fn)=== '[object Function]';
-	};
-	var toCertainPage = function () {
-		var page = parseInt(input.val());
-  		if(isNumber(page) && page > 0 && page <= options.maxPage){
-  			refreshItems(page);
-	  		options.callback(page);
-  		}
-	}
-  	options = $.extend(defaultOption,options);
-  	var init = function () {
-  		//保证options各参数有效性
-  		if(!isNumber(options.maxPage = parseInt(options.maxPage)) || options.maxPage < 1){
-  			options.maxPage = 1;
-  		}
-  		if(!isNumber(options.currentPage = parseInt(options.currentPage)) || options.currentPage < 1 || options.currentPage > options.maxPage){
-  			options.currentPage = 1;
-  		}
-  		if(!isFunction(options.callback)){
-  			options.callback = function () {};
-  		}
-  		total = $('<span class="total">共</span><span class="text">'+options.maxPage+'</span><span class="text">页,</span>');
-  		//显示分页
-  		self.append(prev,leftDot,items,rightDot,next,total,'<span class="text">到第</span>',input,'<span class="text">页</span>',confirm);
-  		//更新分页
-  		refreshItems(options.currentPage);
-  		//监听点击事件
-  		self.on("click","a",function (event) {
-  			var action = this.dataset.action,
-  				page;
-  			switch (action){
-  				case "previous" :
-  					page = options.currentPage - 1;break;
-  				case "next" :
-  					page = options.currentPage + 1;break;
-  				default :
-  					if(!isNaN(action = parseInt(action))){
-  						page = action;
-  					}
-  			}
-  			if(page >= 1 && page <= options.maxPage && page != options.currentPage){
-	  			refreshItems(page);
-	  			options.callback(page);
-	  		}
-  			event.preventDefault();
-  		});
-  		input.on("keypress",function () {
-  			if(event.keyCode == "13"){
-  				toCertainPage();
-  			}
-  		});
-  		 confirm.on("click",function () {
-  			toCertainPage();
-  		});
-  	};
-  	var refreshItems = function (page) {
-  		var i,start,end;
-  		//忽略maxPage小于page的情况
-  		if(options.maxPage < page) {
-  			return false;
-  		}
-  		//判断显示哪几页
-  		if(options.maxPage <= 5) {
-  			//总页数小于6页的情况下全部显示
-  			displayItem(1,options.maxPage);
-  		}
-  		else {
-  			//总页数大于5页
-  			start = page - 2;
-  			end = page + 2;
-  			while(start < 1){
-  				start++;
-  				end++;
-  			}
-  			while(end > options.maxPage){
-  				start--;
-  				end--;
-  			}
-  			if(start > 1){
-  				leftDot.css("display","inline");
-  			}
-  			else {
-  				leftDot.css("display","none");
-  			}
-  			if(end < options.maxPage){
-  				rightDot.css("display","inline");
-  			}
-  			else {
-  				rightDot.css("display","none");
-  			}
-  			displayItem(start,end);
-  		}
-  		options.currentPage = page;
-  		//改变样式
-  		if(page == 1){
-  			prev.addClass("disabled");
-  		}
-  		else {
-  			prev.removeClass("disabled");
-  		}
-  		if(page == options.maxPage){
-  			next.addClass("disabled");
-  		}
-  		else {
-  			next.removeClass("disabled");
-  		}
-  		$('[data-action="'+page+'"]').addClass("disabled");
-  	};
-  	var displayItem = function (start,end) {
-  		var content = '';
-  		for (var i = start; i <= end; i++) {
-  			content += options.item.replace(/\{page\}/g,i);
-  		}
-  		items.html(content);
-  	};
-  	init();
+   /*** jQuery plugin facade ***/
+  $.fn.jPagination = function(method,options) {
+    if(typeof options === 'object'){
+      if(method && methods[method]){
+        methods[method].call(this,options);
+        return;
+      }
+    }
+    else if(typeof method === 'object'){
+        methods.init.call(this,method);
+        return;
+    }
+    $.error('Method ' + method + ' does not exist on jquery.jPagination');
   };
 })(jQuery);
